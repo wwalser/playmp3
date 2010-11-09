5ecaf039bb4f1a6ed70d53b24d0e69016d7cbc65
Wesley Walser <waw325@gmail.com>
2010-11-08 22:57:14 -0500

    added richcombo functionality and styles

--- playmp3s.js
+++ playmp3s.js
@@ -3,14 +3,24 @@ var player = document.createElement('div'),
 	songList;
 	
 player.innerHTML = '<audio></audio><select class="playMp3SongList"></select>';
-songList = player.lastChild;
+songList = $(player.lastChild);
 	
 for (var i = 0; i < playmp3s.mp3Anchors.length; i++) {
 	var songOption = document.createElement('option');
 	anchor = playmp3s.mp3Anchors[i];
 	songOption.setAttribute('value', anchor.getAttribute('href'));
 	songOption.textContent = anchor.textContent;
-	songList.insertBefore(songOption);
+	songList.append(songOption);
 }
-document.body.insertBefore(player);
+
+$(player).appendTo('body');
+songList.richcombo().richcombo('refresh');
+var playerDropDown = songList.richcombo('widget');
+playerDropDown.position({
+	my: "right top",
+	at: "right top",
+	of: window,
+	collision: "none"
+});
+
 })();
