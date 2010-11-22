ce6e50d8984bd2b0cee4fa643e06bc6151e1f004
Wesley Walser <waw325@gmail.com>
2010-11-21 23:18:07 -0500

    fixed the multi-binding of the page action click event. Now hides the pageaction after click. Iterated the version number

--- contentscript.js
+++ contentscript.js
@@ -1,15 +1,14 @@
 var playmp3s = {};
+
 (function(){
 var anchors = document.getElementsByTagName('a'),
 	mp3Regex = new RegExp('\\.mp3$'),
 	mp3Anchors = [];
-
 function notify(mp3Anchors){
 	//notify background of the match and wait for a click on the page action
 	playmp3s.mp3Anchors = mp3Anchors;
 	chrome.extension.sendRequest({});
 }
-
 
 for (var i = 0; i < anchors.length; i++) {
 	var anchor = anchors[i];
